09ec731d017df8aa5d9b40abe461f37c36cf6b27
Marco Signoretto <marco.signoretto@careem.com>
2020-09-01 11:31:06 +0200

    other experiments

--- buildSrc/src/main/kotlin/jojo/MyType.kt
+++
@@ -1,3 +0,0 @@
-package jojo
-
-interface MyType
---
+++ mockingbird-compiler/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    `kotlin-dsl`
+}
+
+gradlePlugin {
+    plugins {
+        register("mockingbird") {
+            id = "com.careem.mockingbird"
+            implementationClass = "com.careem.mockingbird.MockingbirdPlugin"
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+    google()
+    jcenter()
+    gradlePluginPortal()
+}
+
+kotlin {
+    // Add Deps to compilation, so it will become available in main project
+    sourceSets.getByName("main").kotlin.srcDirs("buildSrc/src/main/kotlin")
+    println("ROOT:${rootProject.allprojects}")
+}
+
+dependencies {
+    implementation(Deps.kotlin.plugin)
+    implementation(Deps.square.kotlinPoet)
+    implementation(Deps.square.kotlinPoetMetadata)
+    implementation(Deps.square.kotlinPoetMetadataSpecs)
+    implementation(Deps.kotlinx.metadata)
+}
+
+//task pluginVersion {
+//    def outputDir = file("gen")
+//
+//    inputs.property 'version', version
+//    outputs.dir outputDir
+//
+//    doLast {
+//        def versionFile = file("$outputDir/com/squareup/sqldelight/Version.kt")
+//        versionFile.parentFile.mkdirs()
+//        versionFile.text = """// Generated file. Do not edit!
+//package com.squareup.sqldelight
+//
+//val VERSION = "${project.version}"
+//"""
+//    }
+//}
+//
+//tasks.getByName('compileKotlin').dependsOn('pluginVersion')
+//
+//apply from: "$rootDir/gradle/gradle-mvn-push.gradle"
+
