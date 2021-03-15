e27c69b08cedc4a443add754419081e64b7f450b
Nikolay Kasyanov <nikolay.kasyanov@careem.com>
2021-03-15 11:33:02 +0100

    Add a type safe invocation argument accessor

--- mockingbird/src/commonMain/kotlin/com/careem/mockingbird/test/Invocation.kt
+++ mockingbird/src/commonMain/kotlin/com/careem/mockingbird/test/Invocation.kt
@@ -3,4 +3,14 @@ package com.careem.mockingbird.test
 data class Invocation(
     val methodName: String,
     val arguments: Map<String, Any?>
-)
+) {
+    /**
+     * Returns the value of argument with given [name] cast to [ArgumentType].
+     *
+     * @throws [kotlin.ClassCastException] if value cannot be cast to [ArgumentType].
+     */
+    inline fun <reified ArgumentType> getArgument(name: String): ArgumentType {
+        @Suppress("UNCHECKED_CAST")
+        return arguments[name] as ArgumentType
+    }
+}
